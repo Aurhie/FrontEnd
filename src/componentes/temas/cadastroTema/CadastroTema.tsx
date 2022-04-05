98e8917437c5f227c168c387cea8b40a98ecce8d
mariisneves <mariana.aneves@outlook.com>
2022-04-05 10:53:26 -0300

    Implementado CadastroTema

---
+++ src/componentes/temas/cadastroTema/CadastroTema.tsx
@@ -0,0 +1,118 @@
+import { Button, Container, TextField, Typography } from '@material-ui/core';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import Tema from '../../../models/Tema';
+import { buscaId, post, put } from '../../../service/Service';
+
+function CadastroTema() {
+    let history = useHistory();
+
+    const { id } = useParams<{ id: string }>();
+
+    const [token, setToken] = useLocalStorage("token");
+
+    const [tema, setTema] = useState<Tema>({
+        id: 0,
+        nome: "",
+        descricao: ""
+    })
+
+    useEffect(() => {
+        if (token === "") {
+            alert("Você precisa estar logado")
+            history.push("/login")
+        }
+    }, [token])
+
+    useEffect(() => {
+        if(id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+                "Authorization": token
+            }
+        })
+    }
+
+    function updatedTema(e: ChangeEvent<HTMLInputElement>) {
+        setTema({
+            ...tema,
+            [e.target.name]: e.target.value,
+            postagem: [{}]
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if(id !== undefined) {
+            try {
+                await put(`/temas`, tema, setTema, {
+                    headers: {
+                        "Authorization": token
+                    }
+                })
+                alert("Tema atualizado com sucesso");
+            } catch (error) {
+                console.log(`Error: ${error}`)
+                alert("Erro, por favor verifique a quantidade mínima de caracteres")
+            }
+        } else {
+            try {
+                await post(`/temas`, tema, setTema, {
+                    headers: {
+                        "Authorization": token
+                    }
+                })
+                alert("Tema cadastrado com sucesso");
+            } catch (error) {
+                console.log(`Error: ${error}`)
+                alert("Erro, por favor verifique a quantidade mínima de caracteres")
+            }
+        }
+
+        back()
+    }
+
+    function back() {
+        history.push("/temas")
+    }
+
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
+                <TextField
+                    value={tema.nome}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)}
+                    id="nome"
+                    label="nome"
+                    variant="outlined"
+                    name="nome"
+                    margin="normal"
+                    fullWidth
+                />
+                <TextField
+                    value={tema.descricao}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTema(e)}
+                    id="descricao"
+                    label="descricao"
+                    variant="outlined"
+                    name="descricao"
+                    margin="normal"
+                    fullWidth
+                />
+                <Button type="submit" variant="contained" color="primary">
+                    Finalizar
+                </Button>
+            </form>
+        </Container>
+    )
+}
+
+export default CadastroTema;
