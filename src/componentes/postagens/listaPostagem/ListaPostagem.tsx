37aa8a806b418156445c94f7f38c7256cfa81351
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-04-05 10:11:03 -0300

    iniciado o codigo do arquivo ListaPostagem

---
+++ src/componentes/postagens/listaPostagem/ListaPostagem.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import Typography from '@mui/material/Typography'
+
+
+function ListaPostagem() {
+    const [postagens, setPostagens] = useState<Postagem[]>([])
+
+    const [token, setToken] = useLocalStorage('token');
+
+    let history = useHistory();
+}
+
+useEffect(() => {
+    if (token == '') {
+        alert('Você precisa estar logado')
+        history.push('/login')
+    }
+}, [token])
+
+async function getPost() {
+    await busca('/postagens', setPostagens, {
+        headers: {  
+            'Authorization': token
+        }
+    })
+}
+
+useEffect(() => {
+    getPost()
+
+}, [postagens.length])
+
+return (
+    <>
+        {
+            postagens.map(postagem => (
+                <Box m={2}>
+                    <Card variant='outlined'>
+                        <Cardcontent>
+                            <Typography gutterBottom>
+                                Postagens
+                            </Typography>
+
+                            <Typography variant='h5' component='h2'>
+                                {postagem.titulo}
+                            </Typography>
+
+                            <Typography variant='body2' component='p2'>
+                                {postagem.texto}
+                            </Typography>
+
+                            <Typography variant='body2' component='p2'>
+                                {postagem.tema?.descricao}
+                            </Typography>
+                        </Cardcontent>
+
+                        <CardActions>
+                            <Box display='flex' justifyContent='center' mb={1.5}>
+                                <Link to={`/formularioPostagem/${postagem.id}`}></Link>
+                            </Box>
+                        </CardActions>
+                    </Card>
+                </Box>
+            ))
+        }
+    </>
+)
