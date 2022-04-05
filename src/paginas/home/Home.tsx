477d668d90c98c437a5adecc98728d89ae395ea8
Aurora Marques <Auroras321@hotmail.com>
2022-04-05 15:45:39 -0300

    Ajuste no botão ver postagens e nova postagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -27,18 +27,20 @@ function Home() {
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
                         <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
-                    
-                    <Link to='/postagens'>
-                        <Box display="flex" justifyContent="center">
-                       
-                            <Box marginRight={1}>
-                                <ModalPostagem />
-                            </Box>
-                            <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3f51b5", color: "white" }}>
-                                        Ver Postagens
-                            </Button>
+
+
+                    <Box display="flex" justifyContent="center">
+
+                        <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
-                    </Link>
+                        <Link to='/postagens' className='text-decorator-none'>
+                            <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3f51b5", color: "white" }}>
+                                Ver Postagens
+                            </Button>
+                        </Link>
+                    </Box>
+
                 </Grid>
                 <Grid item xs={6}>
                     <img src="https://i.imgur.com/KDZ0exd.png" alt="" width="500px" height="400px" />
