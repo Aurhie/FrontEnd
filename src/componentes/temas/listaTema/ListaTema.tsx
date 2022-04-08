d107fc39e9514ccd2f97e25faa3f75c22eed4341
wowilliam <wo.william@hotmail.com>
2022-04-08 15:28:37 -0300

    lista add

--- src/componentes/temas/listaTema/ListaTema.tsx
+++ src/componentes/temas/listaTema/ListaTema.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardActions, CardContent, Typography, FormControlLabel, Checkbox } from "@material-ui/core";
 import Tema from "../../../models/Tema";
 import './ListaTema.css';
 
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { busca } from "../../../service/Service";
 import { useSelector } from "react-redux";
 import { UserState } from "../../../store/tokens/UserReducer";
 import { toast } from 'react-toastify';
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
 
 function ListaTema() {
 
     const [temas, setTemas] = useState<Tema[]>([])
     let history = useHistory();
-    const token = useSelector < UserState, UserState["tokens"]> (
+    const token = useSelector<UserState, UserState["tokens"]>(
         (state) => state.tokens
     );
 
@@ -29,7 +30,7 @@ function ListaTema() {
                 draggable: false,
                 theme: "dark",
                 progress: undefined,
-                });
+            });
             history.push('/login')
         }
     }, [token])
@@ -50,14 +51,15 @@ function ListaTema() {
 
     return (
         <>
+            <Typography gutterBottom>
+                Tema
+            </Typography>
             {
                 temas.map(tema => (
                     <Box m={2}>
-                        <Card variant='outlined'>
+                        <Card variant='outlined' className="caixa-post2">
                             <CardContent>
-                                <Typography gutterBottom>
-                                    Tema
-                                </Typography>
+
 
                                 <Typography variant="h5" component='h2'>
                                     {tema.nome}
@@ -69,23 +71,32 @@ function ListaTema() {
 
                             </CardContent>
 
-                            <CardActions>
-                                <Box display='flex' justifyContent='center' mb={1.5}>
-                                    <Link to={`/formularioTema/${tema.id}`} className='text-decorator-none'>
-                                        <Box mx={1}>
-                                            <Button className='btn-atualizar'>
-                                                Atualizar
-                                            </Button>
-                                        </Box>
-                                    </Link>
+                            <CardActions className="chari2">
+                                <Box display='flex' justifyContent='center' alignItems="center" mb={1.5}>
 
-                                    <Link to={`/deletarTema/${tema.id}`} className='text-decorator-none'>
-                                        <Box mx={1}>
-                                            <Button className='btn-deletar'>
-                                                Deletar
-                                            </Button>
-                                        </Box>
-                                    </Link>
+                                    <Box display='flex' justifyContent='center' alignItems="center">
+                                        <FormControlLabel
+                                            control={<Checkbox icon={<FavoriteBorder />}
+                                                checkedIcon={<Favorite />}
+                                                name="checkedH" />}
+                                            label="Curtir"
+                                        />
+                                        <Link to={`/formularioTema/${tema.id}`} className='text-decorator-none'>
+                                            <Box mx={1}>
+                                                <Button className='btn-atualizar'>
+                                                    Atualizar
+                                                </Button>
+                                            </Box>
+                                        </Link>
+
+                                        <Link to={`/deletarTema/${tema.id}`} className='text-decorator-none'>
+                                            <Box mx={1}>
+                                                <Button className='btn-deletar'>
+                                                    Deletar
+                                                </Button>
+                                            </Box>
+                                        </Link>
+                                    </Box>
                                 </Box>
                             </CardActions>
                         </Card>
