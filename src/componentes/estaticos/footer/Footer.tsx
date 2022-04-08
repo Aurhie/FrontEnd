74f1585a6277696c47807f3d59f75fbfa1932c68
Aurora Marques <Auroras321@hotmail.com>
2022-04-08 11:47:09 -0300

    Atualização NavBar e footer

--- src/componentes/estaticos/footer/Footer.tsx
+++ src/componentes/estaticos/footer/Footer.tsx
@@ -1,22 +1,31 @@
-import react from 'react';
+import React, { useState } from "react";
+import { Box, Grid, Typography } from "@material-ui/core";
 import GitHubIcon from '@material-ui/icons/GitHub';
+import './Footer.css'
+import { useDispatch, useSelector } from "react-redux";
+import { UserState } from "../../../store/tokens/UserReducer";
+import { addToken } from "../../../store/tokens/actions";
 
-import {Typography, Box, Grid } from '@material-ui/core';
-import './Footer.css';
+function Footer() {
+    const token = useSelector<UserState, UserState['tokens']>(
+        (state) => state.tokens
+    )
 
+    const dispatch = useDispatch()
 
-function Footer()
-{
-return (
-    <>
+    dispatch(addToken(token))
+
+    var footerComponent;
+    if (token != "") {
+        footerComponent=
         <Grid container direction="row" justifyContent="center" alignItems="center">
             <Grid alignItems="center" item xs={12}>
                 <Box className='box1'>
                     <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                         <Typography variant="h5" align="center" gutterBottom className='text'>Nos siga nas redes sociais! </Typography>
                     </Box>
-                    <Box display="flex" alignItems="center" justifyContent="center"> 
-                    <a href="https://github.com/ColaboraTechPI" target="_blank">
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <a href="https://github.com/ColaboraTechPI" target="_blank">
                             <GitHubIcon className='redes' />
                         </a>
                     </Box>
@@ -26,13 +35,17 @@ return (
                         <Typography variant="subtitle2" align="center" gutterBottom className='text' >Copyright © 2022 by Colaboratech</Typography>
                     </Box>
                     <Box>
-                            <Typography variant="subtitle2" gutterBottom className='text' align="center">All Rights reserved</Typography>
+                        <Typography variant="subtitle2" gutterBottom className='text' align="center">All Rights reserved</Typography>
                     </Box>
                 </Box>
             </Grid>
         </Grid>
-    </>
-)
+    }
+    return (
+        <>
+            {footerComponent}
+        </>
+    )
 }
 
 export default Footer;
--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import react from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-import {useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import './Navbar.css';
 
@@ -10,18 +10,18 @@ import { useSelector } from 'react-redux';
 import { UserState } from '../../../store/tokens/UserReducer';
 import { useDispatch } from "react-redux";
 import { addToken } from '../../../store/tokens/actions';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 
 function Navbar() {
 
     const token = useSelector<UserState, UserState["tokens"]>(
         (state) => state.tokens
-      );
+    );
     let history = useHistory();
     const dispatch = useDispatch();
-    
-    
-    function goLogout(){
+
+
+    function goLogout() {
         dispatch(addToken(''));
         toast.info('Usuário deslogado', {
             position: "top-right",
@@ -38,68 +38,74 @@ function Navbar() {
 
     var navbarComponent;
 
-    if(token != ""){
+    if (token != "") {
         navbarComponent = <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box mx={1} className='cursor logo'>
-                        
-                       
-                    </Box>
-                    
-                    <Box display='flex' justifyContent='start'>
-                        <Link to='/home' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    Home
-                                </Typography>
-                            </Box>
-                        </Link>
+            <Toolbar variant="dense" className='navCor'>
+                <Box mx={1} className='cursor logo'>
 
-                        <Link to='/postagens' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    Postagens
-                                </Typography>
-                            </Box>
-                        </Link>
 
-                        <Link to='/temas' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    Temas
-                                </Typography>
+                </Box>
 
-                            </Box>
-                        </Link>
+                <Box display='flex' justifyContent='start' alignItems='center'>
+                    <Link to='/home' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                        <Link to='/formularioTema' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    Cadastrar tema
-                                </Typography>
-                            </Box>
-                        </Link>
+                    <Link to='/postagens' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" color="inherit">
+                                Postagens
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                        <Link to='/sobre-nos' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    Sobre nós
-                                </Typography>
-                            </Box>
-                        </Link>
+                    <Link to='/temas' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" color="inherit">
+                                Temas
+                            </Typography>
 
-                        <Link to='/login' className='text-decorator-none'>
+                        </Box>
+                    </Link>                    
+
+                    <Link to='/sobre-nos' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" color="inherit">
+                                Sobre nós
+                            </Typography>
+                        </Box>
+                    </Link>
+
+
+                    {/* <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} style={{ cursor: "pointer", color: 'white' }} onClick={goLogout} >
                                 <Typography variant="h5" color="inherit">
                                     Logout
                                 </Typography>
                             </Box>
-                        </Link>
-                        
-                    </Box>
+                        </Link> */}
 
-                </Toolbar>
-            </AppBar >
+                </Box>
+                    {/* Barra de pesquisa */}
+                    <div className='container'>
+            <div id="searchWrapper">
+                <input
+                    type="text"
+                    name="searchBar"
+                    id="searchBar"
+                    placeholder="O que procura?"
+                />
+            </div>
+            <ul id="charactersList"></ul>
+        </div>
+        <script src="app.js"></script>
+
+            </Toolbar>
+        </AppBar >
     }
 
     return (
